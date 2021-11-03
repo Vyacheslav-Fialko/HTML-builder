d89ec3b2132f84d9462b041661543b47462e47d5
Vyacheslav-Fialko <vyacheslav1@ukr.net>
2021-11-03 22:19:13 +0200

    feat: add my code

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const srcFolder = path.resolve(__dirname, 'files');
+const distFolder = path.resolve(__dirname, 'files-copy');
+let isDist = fs.existsSync(distFolder);
+
+
+fs.readdir(srcFolder, (err, files) => {
+  if (err) console.log(err);
+  else {
+    if (!isDist) {
+      fs.mkdir(distFolder, (err) => {
+        if (err) {
+          return console.error(err);
+        }
+      });
+    }
+    files.forEach((file) => {
+      fs.copyFile(path.resolve(srcFolder, file), path.resolve(distFolder, file), (err) => {
+        if (err) {
+          console.log(err);
+          }
+        });
+      });
+  }
+});
 
