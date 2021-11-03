48041d7b2ed6bbdd76b3e3584528b8fc2942e976
Vyacheslav-Fialko <vyacheslav1@ukr.net>
2021-11-03 22:24:28 +0200

    feat: add corect output

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -8,13 +8,12 @@ fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
       if (file.isFile()) {
         const fileName = path.parse(`${folderPath}/${file.name}`).name;
         const fileExt = path.parse(`${folderPath}/${file.name}`).ext.slice(1);
-        const fileF = path.parse(`${folderPath}/${file.name}`).base;
         fs.stat(`${folderPath}/${file.name}`, (err, stats) => {
           if (err) {
             console.error(err);
             return;
           }
-          console.log(`${fileName}-${fileExt}-${stats.size/1024}.kb`);
+          console.log(`${fileName} - ${fileExt} - ${stats.size/1024}.kb`);
         });
 
       };
